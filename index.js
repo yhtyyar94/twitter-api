8b8b97d52895079ede9dc250b8b049ccb994d6d6
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2023-04-26 19:12:08 +0200

    version 2

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const app = express();
+require("dotenv").config();
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
-
-// view engine setup
-
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 let isStarted = false;
 
@@ -25,25 +28,11 @@ if (!isStarted) {
   isStarted = true;
 }
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+//Routes
+app.use("/", (req, res) => {
+  res.send("Hello World");
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-app.listen(3000, () => {
-  console.log("server started");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
